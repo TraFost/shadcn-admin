21fba762571c88bfffcbdd2836f99d6ab1d2e854
Rahman <rahwisdilfiqrak@gmail.com>
2024-07-02 08:13:02 +0700

    feat(toaster): implement toast

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Toaster } from 'sonner'
 import { RouterProvider } from 'react-router-dom'
-import { Toaster } from '@/components/ui/toaster'
 import { ThemeProvider } from '@/components/theme-provider'
 import router from '@/router'
 import '@/index.css'
