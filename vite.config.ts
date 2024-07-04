ba3aabd441b90616dcf2e04899b12656f3c02795
Rahman <rahwisdilfiqrak@gmail.com>
2024-07-05 01:19:09 +0700

    feat: add million lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
+import MillionLint from '@million/lint'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+const _plugins = [react()]
+_plugins.unshift(MillionLint.vite() as PluginOption[])
 export default defineConfig({
-  plugins: [react()],
+  plugins: _plugins,
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
